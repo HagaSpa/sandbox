cc226f18afbc64d995f94188d4ff18da44490e0e
HagaSpa <justicesparrow13@gmail.com>
2018-02-01 22:15:54 +0900

    Vue Componentの実装

--- VueTest/app.js
+++ VueTest/app.js
@@ -1,4 +1,28 @@
 window.addEventListener('DOMContentLoaded', () => {
+
+    // Component定義
+    Vue.component('task-card', {
+        props: ['task'],
+        template: `<div class="card">
+        <div class="card-content">
+          {{ task.name }}
+        </div>
+        <footer class="card-footer">
+          <div class="card-footer-item">
+            {{ task.assignee }}
+          </div>
+          <div class="card-footer-item">
+            {{ task.mandays }} 人日
+          </div>
+        </footer>
+        <footer class="card-footer">
+          <a class="card-footer-item">◀︎</a>
+          <a class="card-footer-item">▶︎</a>
+        </footer>
+      </div>`
+    });
+
+    // Vueインスタンス生成
     new Vue({
         el: '#board',
         data: {
@@ -8,6 +32,24 @@ window.addEventListener('DOMContentLoaded', () => {
             { name: 'task 3', status: 2, assignee: '🐱', mandays: 1 },
             { name: 'task 4', status: 3, assignee: '🐹', mandays: 1 }
           ]
+        },
+        computed: {
+            tasksOpen: function(tasks) {
+                return this.tasks.filter(task => {
+                    return task.status === 1;
+                })
+            },
+            tasksDoing: function(tasks) {
+                return this.tasks.filter(task => {
+                    return task.status === 2;
+                })
+            },
+            tasksClosed: function(tasks) {
+                return this.tasks.filter(task => {
+                    return task.status === 3;
+                })
+            }
         }
-    })
+    });
+
 });
