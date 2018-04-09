c5d327a81e9eb126b700d29557663751044c9dff
HagaSpa <justicesparrow13@gmail.com>
2018-04-09 21:54:14 +0900

    セレクタを指定してスクレイピング

--- WebScraipingTest/puppeteer_test.js
+++ WebScraipingTest/puppeteer_test.js
@@ -1,10 +1,11 @@
 const puppeteer = require('puppeteer');
-const url = 'https://docs.google.com/spreadsheets/d/1e8Ott5IfoyXaOcaGQfEW4gxbylA89mF9SB2An2M3gFY/edit#gid=1088598455';
+const url = 'https://toolassisted.github.io/OKI/#DSM/57/8/8/1,0,,0,/1,0,,0,/1,0,,0,/1,0,,0,/1,0,,0,/1,0,,0,/1,0,,0,/';
 
 async function getData(page, url){
-  await page.goto(url); // ページへ移動
-  // 任意のJavaScriptを実行
-  return await page.evaluate(() => document.getElementsByClassName('FRAMES'));
+  await page.goto(url, {waitUntil: 'networkidle2',timeout:5000}); // ページへ移動
+  // セレクタを指定してデータ取得
+  return await page.evaluate(
+      () => document.querySelector('body > div.container-fluid > div.frames.index > table > tbody > tr:nth-child(1) > td:nth-child(10)'));
 }
 
 !(async() => {
