5508e173c7fa846de3f4b3f708cbe35bc0b163f3
HagaSpa <justicesparrow13@gmail.com>
2018-07-07 20:25:28 +0900

    各RequestHandler の実装

--- Alexa_noodle_skill/index.js
+++ Alexa_noodle_skill/index.js
@@ -4,19 +4,57 @@ let skill;
 exports.handler = async function (event, context) {
     if (!skill) {
       skill = Alexa.SkillBuilders.custom()
-        .addRequestHandlers(MyHandler)
+        .addRequestHandlers(LaunchRequestHandler,OrderIntentHandler,MenuIntentHandler)
         .create();
     }
     return skill.invoke(event);
 }
- 
-const MyHandler = {
+
+/**
+ * LaunchRequest用ハンドラ。
+ * ※LaunchRequestはalexa側で、設定されたIntent以外の発話内容のすべて
+ * 
+ * canHandle()がtureを返すときのみ、handle()は実行される。
+ */
+const LaunchRequestHandler = {
     canHandle(handlerInput) {
-        return true;
+        return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
     },
     handle(handlerInput) {
         return handlerInput.responseBuilder
-            .speak('こんにちは')
+            .speak('ようこそ、ラーメン出前スキルへ、ご注文をどうぞ')
+            .reprompt('ご注文をお伺いします')
+            .getResponse();
+    }
+};
+ 
+/**
+ * OrderIntent用ハンドラ
+ */
+const OrderIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'OrderIntent';
+    },
+    handle(handlerInput) {
+        return handlerInput.responseBuilder
+            .speak('ご注文ありがとうございました')
+            .getResponse();
+    }
+};
+
+/**
+ * MenuIntent用ハンドラ
+ */
+const MenuIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'MenuIntent';
+    },
+    handle(handlerInput) {
+        return handlerInput.responseBuilder
+            .speak('メニューは、しょうゆラーメン、みそらーめん、とんこつラーメンの三種類あります。何になさいますか？')
+            .reprompt('ご注文をお伺いします')
             .getResponse();
     }
 };
