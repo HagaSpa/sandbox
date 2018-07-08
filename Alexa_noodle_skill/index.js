055a7fec01b9d618a71171eebea0d3d58ba3770d
HagaSpa <justicesparrow13@gmail.com>
2018-07-08 21:46:29 +0900

    会計処理の実装。セッション間では、合計金額を保持するように修正。

--- Alexa_noodle_skill/index.js
+++ Alexa_noodle_skill/index.js
@@ -2,6 +2,29 @@ const Alexa = require('ask-sdk');
 const successSlotsMatch = "ER_SUCCESS_MATCH";      //slotに該当した際、alexaから返却される成功ステータス
 const successSlotsNoMatch = "ER_SUCCESS_NO_MATCH"; //slotに該当しなかった際、alexaから返却される成功ステータス
 
+/**
+ * countのId => Number型 変換マップ
+ */
+const countIdMap = new Map([
+    ["One", 1],
+    ["Two", 2],
+    ["Three", 3],
+    ["Four", 4],
+    ["Five", 5]
+]);
+
+/**
+ * kindOfNoodleのId => 値段 変換マップ
+ */
+const kindOfNoodleIdMap = new Map([
+    ["Soysauce", 700],
+    ["Miso", 800],
+    ["Tonkotsu", 900]
+]);
+
+/**
+ * メイン処理
+ */
 let skill;
 exports.handler = async function (event, context) {
     if (!skill) {
@@ -10,7 +33,8 @@ exports.handler = async function (event, context) {
             LaunchRequestHandler,
             OrderIntentHandler,
             MenuIntentHandler,
-            OrderIntentNoMtchSlotsHandler)
+            OrderIntentNoMatchSlotsHandler,
+            NoIntentHandler)
         .create();
     }
     return skill.invoke(event);
@@ -34,17 +58,34 @@ const LaunchRequestHandler = {
  
 /**
  * OrderIntent用ハンドラ
- * slotが合致した場合実行される。
+ * kindOfNoodleが合致した場合、かつcountも合致した場合
  */
 const OrderIntentHandler = {
     canHandle(handlerInput) {
-        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-            && handlerInput.requestEnvelope.request.intent.name === 'OrderIntent'
-            && handlerInput.requestEnvelope.request.intent.slots.kindOfNoodle.resolutions.resolutionsPerAuthority[0].status.code  === successSlotsMatch;
+        const request = handlerInput.requestEnvelope.request;
+        return request.type === 'IntentRequest'
+            && request.intent.name === 'OrderIntent'
+            && request.intent.slots.kindOfNoodle.resolutions.resolutionsPerAuthority[0].status.code  === successSlotsMatch
+            && request.intent.slots.count.resolutions.resolutionsPerAuthority[0].status.code  === successSlotsMatch;
     },
     handle(handlerInput) {
+        const slots = handlerInput.requestEnvelope.request.intent.slots;
+        // 会計処理
+        const totalFee = toAccount(slots);
+        // セッション情報の取得
+        let attributes = handlerInput.attributesManager.getSessionAttributes();
+        // 合計金額をsessionAttributeへセット
+        if (!attributes.totalFee) { 
+            attributes.totalFee = totalFee;
+        } else {
+            attributes.totalFee += totalFee;
+        }
+        // セッション情報の保存
+        handlerInput.attributesManager.setSessionAttributes(attributes);
+
         return handlerInput.responseBuilder
-            .speak('ご注文ありがとうございました')
+            .speak(`お会計は${attributes.totalFee}円です。他に何か注文しますか？`)
+            .reprompt('ご注文は以上でよろしいでしょうか？')
             .getResponse();
     }
 };
@@ -53,7 +94,7 @@ const OrderIntentHandler = {
  * OrderIntent用ハンドラ。
  * slotsが合致しなかった場合実行される。具体的には3種類のラーメン以外が、注文された場合。
  */
-const OrderIntentNoMtchSlotsHandler = {
+const OrderIntentNoMatchSlotsHandler = {
     canHandle(handlerInput) {
         return handlerInput.requestEnvelope.request.type === 'IntentRequest'
             && handlerInput.requestEnvelope.request.intent.name === 'OrderIntent'
@@ -74,7 +115,7 @@ const OrderIntentNoMtchSlotsHandler = {
 const MenuIntentHandler = {
     canHandle(handlerInput) {
         return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-            && handlerInput.requestEnvelope.request.intent.name === 'MenuIntent';
+            && (handlerInput.requestEnvelope.request.intent.name === 'MenuIntent' || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.YesIntent');
     },
     handle(handlerInput) {
         return handlerInput.responseBuilder
@@ -83,3 +124,37 @@ const MenuIntentHandler = {
             .getResponse();
     }
 };
+
+/**
+ * いいえの場合
+ * 具体的には、OrderIntentHandler の後に使われる想定
+ */
+const NoIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.NoIntent';
+    },
+    handle(handlerInput) {
+        return handlerInput.responseBuilder
+            .speak(`ご注文を承りました。`)
+            .getResponse();
+    }
+};
+
+/**
+ * 会計する
+ * @param {Object} slots 
+ */
+const toAccount = (slots) => {
+    const kindOfNoodleId = slots.kindOfNoodle.resolutions.resolutionsPerAuthority[0].values[0].value.id;
+    const countId = slots.count.resolutions.resolutionsPerAuthority[0].values[0].value.id;
+    // ラーメンの値段
+    const kindOfNoodleValue = kindOfNoodleIdMap.get(kindOfNoodleId); 
+    // 個数
+    const countValue = countIdMap.get(countId);
+    // 金額を作成（値段 * 個数）
+    const fee = kindOfNoodleValue * countValue;
+    // 合計金額へ加算
+    //totalFee += fee;
+    return fee;
+}
