8e45527cbd6fb3e53b812522d63abe88194576a6
HagaSpa <justicesparrow13@gmail.com>
2020-07-15 14:30:24 +0900

    Update algorithm for q4

--- programmer/q4.py
+++ programmer/q4.py
@@ -4,11 +4,35 @@ def maximum_number(li):
     与えられた正の整数リストから、結合して作成可能な最大数を返す
     """
 
-    # liの中から一番左の桁に対して、降順で並び替えて要素ごとに文字列化
+    # パディングする際の桁数把握のため、要素内の最大桁数を取得
+    m_digit = 0
+    for l in li:
+        if m_digit < len(str(l)):
+              m_digit = len(str(l))
+
+    # 比較を行う。最大桁数を満たしていない要素は比較時にパディング処理を行う。
     for i in range(len(li)):
         for j in range(i+1, len(li)):
-            # 隣の要素の方が一番左端の数字が大きければ並び替える or 一番左の桁が同じ数の場合intで比較して並び替える
-            if int(str(li[i])[0]) < int(str(li[j])[0]) or li[i] < li[j]:
+            comp_i = 0 # 比較用整数
+            comp_j = 0
+            s_i = str(li[i])
+            s_j = str(li[j])
+
+            if len(s_i) < m_digit:
+                d = m_digit - len(s_i)
+                trans = s_i[0] * d  #一番左端の桁数を最大桁数分パディングする
+                comp_i = int(s_i+trans)
+            else:
+                comp_i = li[i]
+            if len(s_j) < m_digit:
+                d = m_digit - len(s_j)
+                trans = s_j[0] * d
+                comp_j = int(s_j+trans)
+            else:
+                comp_j = li[j]
+
+            # 比較してソート
+            if comp_i < comp_j:
                 tmp = li[j]
                 li[j] = li[i]
                 li[i] = tmp
@@ -22,4 +46,5 @@ def maximum_number(li):
 
 print(maximum_number([50, 2, 1, 9]))  # 95021
 print(maximum_number([50, 52, 1, 9])) # 952501
-print(maximum_number([5, 50, 56]))    # 56550 じゃなきゃダメじゃね？
+print(maximum_number([5, 50, 56]))    # 56550
+print(maximum_number([420, 42, 423])) # 42423420
